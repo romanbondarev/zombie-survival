ef14f5cf46f3e6a9887f03ce9b8fb38feed86fbf
Roman Bondarev <robond@ttu.ee>
2018-09-12 13:10:24 +0300

    Added excuses for future me!

--- core/src/com/gdx/game/Application.java
+++ core/src/com/gdx/game/Application.java
@@ -25,7 +25,6 @@ import static com.gdx.game.utils.Constants.SCALE;
  * 19.08.2018
  */
 
-
 public class Application extends ApplicationAdapter {
     private OrthographicCamera camera;
     private SpriteBatch batch;
